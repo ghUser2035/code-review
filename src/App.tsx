e467e3e9a161191a52985f3a6474719da94a2766
ghUser2035 <94993079+ghUser2035@users.noreply.github.com>
2024-08-14 16:51:41 +0300

    Bad code (#2)
    Co-authored-by: Alex <oleksand.hurov@gen.tech>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,57 @@
-import { useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const SECRET_TO_AWS = 'secret GIJGHNFGJKHNFGJKHNFGHNFJGHNKJGFNH'
+
+function App(): ReactElement {
+  const [count, setCount] = useState<number>(0)
+  const [secret, setSecret] = useState<string>(null);
+
+
+  const interval = setInterval(function() {
+    console.log("I'm alive!!");
+  }, 200);
+
+  const displaySum = (stopAt: number): void => {
+    let sum = 0;
+
+    for (let i = 0; i < stopAt; i++) {
+      if (i % 2 === 0) {
+        sum += i;
+      }
+    }
+
+    console.log('sum === %s', sum);
+    clearInterval(interval);
+  };
+
+
+  console.time('sum took');
+  displaySum(199999999);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  useEffect(() => {
+    setSecret(SECRET_TO_AWS);
+  }, []);
+
+  console.log(secret);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-2xl text-4xl leading-bug mb-10">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
